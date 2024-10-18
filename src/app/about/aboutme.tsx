79f6e58bf2ea4f3d14af11dd9215bd1effbe0a8f
Alan Grinberg <grinny@icloud.com>
2024-10-18 13:25:09 -0500

    fix quotes

--- src/app/about/aboutme.tsx
+++ src/app/about/aboutme.tsx
@@ -119,14 +119,15 @@ const ContactForm = () => {
 };
 
 const AboutMe: React.FC = () => {
+  const aboutText = `Hello! I'm a passionate developer with experience in ReactJS, HTML/CSS, Java, SQL after my 4 years of working over at GM on
+  the E-Commerce side of the business.
+  I love working on all sorts of web development and am always eager to learn new technologies.
+  When I'm not coding, you can find me playing music, tennis, hanging out at the latest food trucks and food spots out in Austin.`;
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold text-center mb-5">About Me</h1>
       <p className="text-lg mb-10 text-center max-w-[100vw]">
-        Hello! I'm a passionate developer with experience in ReactJS, HTML/CSS, Java, SQL after my 4 years of working over at GM on
-        the E-Commerce side of the business.
-        I love working on all sorts of web development and am always eager to learn new technologies.
-        When I'm not coding, you can find me playing music, tennis, hanging out at the latest food trucks and food spots out in Austin.
+        {aboutText}
       </p>
       <div className="mb-6">
         <Image
